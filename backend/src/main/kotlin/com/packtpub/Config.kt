47f14e653f4fdfe53c19076f49ceee56f05a62ce
Jussi Hallila <Jussi@Hallila.com>
2017-08-10 19:29:30 +0100

    Section 1, Video 3. Functional bean definitions.

--- backend/src/main/kotlin/com/packtpub/Config.kt
+++ backend/src/main/kotlin/com/packtpub/Config.kt
@@ -3,6 +3,9 @@ package com.packtpub
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.beans
 
 
 @SpringBootApplication
@@ -15,5 +18,11 @@ open class Config(
 }
 
 fun main(args: Array<String>){
-    SpringApplication.run(Config::class.java, *args)
+    val application = SpringApplication(Config::class.java)
+    application.addInitializers(ApplicationContextInitializer<GenericApplicationContext> { ctx ->
+        beans{
+            bean<HelloWorldConfig>()
+        }(ctx)
+    })
+    application.run(*args)
 }
--- backend/src/main/kotlin/com/packtpub/HelloWorld.kt
+++ backend/src/main/kotlin/com/packtpub/HelloWorld.kt
@@ -1,8 +1,5 @@
 package com.packtpub
 
-import org.springframework.stereotype.Service
-
-@Service
 open class HelloWorld{
     fun sayHello(){
         println("Hello World")
