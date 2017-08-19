0fa2ed1e9b91a8c26109fa5c8fe6fdd5595525dc
Jussi Hallila <Jussi@Hallila.com>
2017-08-19 12:14:01 +0100

    Section 2, video 2: Kotlin HTML DSL and Spring Controllers

--- backend/api/src/main/kotlin/com/packtpub/HelloController.kt
+++ backend/api/src/main/kotlin/com/packtpub/HelloController.kt
@@ -1,12 +1,49 @@
 package com.packtpub
 
+import kotlinx.html.*
+import kotlinx.html.stream.createHTML
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController(private val helloSayer: HelloSayer) {
-    @GetMapping
-    fun sayHello(@RequestParam("name") name: String): String =
-        helloSayer.sayHello(name)
+
+    val links = mapOf(
+        "Kotlin" to "https://github.com/JetBrains/kotlin",
+        "Spring" to "https://github.com/spring-projects/spring-framework",
+        "React" to "https://github.com/facebook/react",
+        "Full Stack Development" to "https://github.com/Xantier/fullstack-kotlin"
+    )
+
+    @GetMapping(produces = arrayOf("text/html"))
+    fun sayHello(@RequestParam(value = "name", required = false) name: String?)
+        : String = createHTML(true).html {
+        head {
+            title = "Full Stack Kotlin"
+        }
+        body {
+            h4 {
+                +helloSayer.sayHello(name ?: "User")
+            }
+            p {
+                +"Welcome to full stack Kotlin"
+            }
+            p {
+                +"Our Resouces: "
+                ul {
+                    links.map { (name, url) ->
+                        li {
+                            a(url) {
+                                target = ATarget.blank
+                                +name
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
 }
