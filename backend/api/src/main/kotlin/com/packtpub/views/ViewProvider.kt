acf9380b9c2e419fcbfc1c250e4e8637e6a3305c
Jussi Hallila <jussi@hallila.com>
2017-09-14 20:49:40 +0100

    Removing extra login form.

--- backend/api/src/main/kotlin/com/packtpub/views/ViewProvider.kt
+++ backend/api/src/main/kotlin/com/packtpub/views/ViewProvider.kt
@@ -32,26 +32,3 @@ fun index(header: String, projects: List<ProjectView>): String {
 
     }
 }
-
-fun login(): String {
-    return createHTML(true).html {
-        head {
-            title = "Full Stack Kotlin - Login"
-            styleLink("/static/css/hello.css")
-        }
-        body {
-            h4 { +"Please Long" }
-            p {
-                form(action = "login") {
-                    input(type = InputType.text, name = "name")
-                    input(type = InputType.password, name = "password")
-                    submitInput(formMethod = InputFormMethod.post)
-                }
-            }
-        }
-
-    }
-}
-
-
-
