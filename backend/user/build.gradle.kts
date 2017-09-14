53d9ffa79bd127778c5d725b25bff40f1d1cb613
Jussi Hallila <jussi@hallila.com>
2017-09-14 21:55:56 +0100

    Section 5, Video 1: Enabling Spring Security to the application.

--- backend/user/build.gradle.kts
+++ backend/user/build.gradle.kts
@@ -4,5 +4,11 @@ plugins {
 }
 
 dependencies {
-
+    val springBootVersion: String = parent.properties["springBootVersion"] as String
+    val springSecurityVersion: String = parent.properties["springSecurityVersion"] as String
+    compile("org.springframework.boot:spring-boot-starter:$springBootVersion")
+    compile("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
+    compile("org.springframework.security:spring-security-core:$springSecurityVersion")
+    compile("org.springframework.security:spring-security-config:$springSecurityVersion")
+    compile("org.springframework.security:spring-security-webflux:$springSecurityVersion")
 }
