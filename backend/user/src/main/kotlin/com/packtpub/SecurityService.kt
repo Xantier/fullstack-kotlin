ed8bb840d891a2767c79f6bfe4c55849301b45d7
Jussi Hallila <jussi@hallila.com>
2017-09-22 07:39:46 +0100

    Section 5, Video 3: Adding security service

---
+++ backend/user/src/main/kotlin/com/packtpub/SecurityService.kt
@@ -0,0 +1,23 @@
+package com.packtpub
+
+import org.springframework.security.authorization.AuthorizationDecision
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.server.authorization.AuthorizationContext
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+
+interface SecurityService {
+    fun isAdmin(authentication: Mono<Authentication>, _ac: AuthorizationContext)
+        : Mono<AuthorizationDecision>
+}
+internal class SecurityServiceImpl: SecurityService {
+
+    override fun isAdmin(authentication: Mono<Authentication>,
+                         _ac: AuthorizationContext):
+        Mono<AuthorizationDecision> {
+        return authentication.flatMap {
+            AuthorizationDecision(it.authorities.map { it.authority }
+                .contains("ADMIN")).toMono()
+        }
+    }
+}
