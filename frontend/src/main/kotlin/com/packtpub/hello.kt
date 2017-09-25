e2df10dd669de867243d0667337bbbb77c2a4974
Jussi Hallila <jussi@hallila.com>
2017-09-25 18:36:28 +0100

    Section 7, Video 3: Kotlin and JavaScript interoperability

--- frontend/src/main/kotlin/com/packtpub/hello.kt
+++ frontend/src/main/kotlin/com/packtpub/hello.kt
@@ -1,7 +1,19 @@
 package com.packtpub
 
+import org.w3c.xhr.XMLHttpRequest
+
 fun main(args: Array<String>) {
     println("hello world")
+    js("""
+console.log('hello from javascript')
+""")
+    val xhr = XMLHttpRequest()
+    xhr.open("GET", "http://localhost:8080/api/projects", true)
+    xhr.onreadystatechange = {
+        println(xhr.response)
+    }
+    xhr.send()
 }
+
 
 
