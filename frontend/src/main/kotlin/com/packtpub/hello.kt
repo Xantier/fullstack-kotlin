f55569a8c0bbf8f7545445b3c72782aa966b263d
Jussi Hallila <jussi@hallila.com>
2017-09-27 07:55:05 +0100

    Section 7, Video 4: Removing unneeded Cors filter after Webpack introduction.

--- frontend/src/main/kotlin/com/packtpub/hello.kt
+++ frontend/src/main/kotlin/com/packtpub/hello.kt
@@ -3,9 +3,9 @@ package com.packtpub
 import org.w3c.xhr.XMLHttpRequest
 
 fun main(args: Array<String>) {
-    println("Hello Kotlin Developers")
+    println("Hello Webpack")
     val xhr = XMLHttpRequest()
-    xhr.open("GET", "http://localhost:8080/api/projects", true)
+    xhr.open("GET", "/api/projects", true)
     xhr.onreadystatechange = {
         println(xhr.response)
     }
