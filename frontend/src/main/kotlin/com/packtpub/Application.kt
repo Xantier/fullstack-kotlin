607d5131b25fad37fda405dc1e4b95ed3d4a838a
Jussi Hallila <jussi@hallila.com>
2017-09-30 18:44:38 +0100

    Section 9, Video 2: Communication patterns between React components.

--- frontend/src/main/kotlin/com/packtpub/Application.kt
+++ frontend/src/main/kotlin/com/packtpub/Application.kt
@@ -2,14 +2,12 @@ package com.packtpub
 
 import kotlinx.html.div
 import kotlinx.html.form
-import kotlinx.html.h1
 import kotlinx.html.label
 import react.RProps
 import react.RState
 import react.ReactComponentSpec
 import react.dom.ReactDOMBuilder
 import react.dom.ReactDOMComponent
-import kotlin.browser.window
 
 
 class Application : ReactDOMComponent<Application.Props, Application.State>() {
@@ -19,43 +17,47 @@ class Application : ReactDOMComponent<Application.Props, Application.State>() {
         state = State()
     }
 
-    override fun componentDidMount() {
-        window.setTimeout({
-            setState {
-                showWarning = true
-            }
-        }, 5000)
-    }
-
     override fun ReactDOMBuilder.render() {
         div {
-            when (state.showWarning) {
-                true -> h1 {
-                    +"""YOU HAVE BEEN LOOKING AT THIS
-                        APPLICATION FOR TOO LONG!"""
-                }
-                else -> div {
-                    form {
-                        div(classes = "form-group") {
-                            label {
-                                attributes["for"] = "numberInput"
-                            }
-                            NumberInput {
-                                value = "4"
-                            }
+            form {
+                div(classes = "form-group") {
+                    label {
+                        attributes["htmlFor"] = "numberInput"
+                    }
+                    NumberInput {
+                        change = {
+                            changeNumberValue(it)
                         }
+                        value = state.numberValue
                     }
-                    HelloComponent {
-                        hello = "Hi"
+                }
+                Button {
+                    change = {
+                        val newValue = state.numberValue + 1
+                        changeNumberValue(newValue)
                     }
-                    WorldComponent {
-                        world = "World"
+                    text = "Increment"
+                }
+                Button {
+                    change = {
+                        val newValue = state.numberValue - 1
+                        changeNumberValue(newValue)
                     }
+                    text = "Decrement"
+                }
+                div {
+                    +"Current Value: ${state.numberValue}"
                 }
             }
         }
     }
 
+    private fun changeNumberValue(newValue: Int) {
+        setState {
+            numberValue = newValue
+        }
+    }
+
     class Props : RProps()
-    class State(var showWarning: Boolean = false) : RState
+    class State(var numberValue: Int = 0) : RState
 }
