d7c46f601cf7d237b1b258d36e8f558f47f83300
Jussi Hallila <jussi@hallila.com>
2018-01-13 08:27:34 +0000

    Upgrading Spring dependencies and Gradle wrapper version

--- backend/component/build.gradle.kts
+++ backend/component/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("org.jetbrains.kotlin.plugin.spring") version "1.1.4"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.2.10"
 }
 
 dependencies {
-    val springBootVersion: String = parent.properties["springBootVersion"] as String
+    val springBootVersion: String = parent!!.properties["springBootVersion"] as String
     compile("org.springframework.boot:spring-boot-starter:$springBootVersion")
 }
